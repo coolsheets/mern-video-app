6284b6202db48e2f373de3c3af88b128651b43fa
Mike Fretwell <mfretwell@mGeo.ca>
2025-04-19 22:39:26 -0600

    gitignored more

--- backend/controllers/testInteractionsController.js
+++
@@ -1,4 +0,0 @@
-// filepath: /home/mfretwell/Documents/InceptionU/projects/mern-video-app/backend/testInteractionsController.js
-import { recordInteraction } from './interactionsController.js';
-
-console.log('InteractionsController imported successfully');
